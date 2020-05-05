a0f7e73a5f627c4feb42e615990d4bc5c35a9ca4
Choi Jiwon <winning1993@naver.com>
2020-05-06 01:09:13 +0900

    중지, 초기화, 재생바 완료

--- index.js
+++ index.js
@@ -4,22 +4,46 @@ progressBar = document.getElementById("progressBar"),
 videoPlayTime = document.getElementById("videoPlayTime"),
 video = document.getElementById("video");
 
+
 function playVid(){
-    const seconds = Math.ceil(video.currentTime);
-    const minutes = Math.floor(seconds/60);
-    videoPlayTime.innerHTML = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    const seconds = Math.floor(video.currentTime%60),
+    minutes = Math.floor(seconds/60);
+    videoPlayTime.innerHTML = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    progressBar.value = video.currentTime;
 }
 
 function handlePlay(){
-
   setInterval(playVid,1000);
-video.play();    
+  video.play();
+  playBtn.innerHTML="일시정지";
+  playBtn.removeEventListener("click", handlePlay);
+  playBtn.addEventListener("click", handlePause);    
+}
+
+function handlePause(){
+    clearInterval(playVid,1000);
+    video.pause();
+    playBtn.innerHTML="재생";
+    playBtn.removeEventListener("click", handlePause);
+    playBtn.addEventListener("click", handlePlay);
+}
+
+function handleStop(){
+  handlePause();
+  video.currentTime = 0;
+}
+
+function handleBar(){
+    video.currentTime = progressBar.value;
+    const seconds = Math.floor(video.currentTime%60),
+    minutes = Math.floor(seconds/60);
+    videoPlayTime.innerHTML = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init(){
     playBtn.addEventListener("click", handlePlay);
     stopBtn.addEventListener("click", handleStop);
-    progressBar.addEventListener("click", handleBar);
+    progressBar.addEventListener("input", handleBar);
 }
 
 init();
